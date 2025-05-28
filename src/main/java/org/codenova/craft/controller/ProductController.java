714b3dd2e06b63054198aa22d5ecaf829857a69a
Boaz11Jachin <sheepo84k@gmail.com>
2025-05-28 17:44:37 +0900

    필요한 원자재 수량 구하기

--- src/main/java/org/codenova/craft/controller/ProductController.java
+++ src/main/java/org/codenova/craft/controller/ProductController.java
@@ -7,12 +7,15 @@ import org.codenova.craft.entity.Product;
 import org.codenova.craft.repository.BomRepository;
 import org.codenova.craft.repository.ProductRepository;
 import org.codenova.craft.response.BomNode;
+import org.codenova.craft.response.Demand;
+import org.codenova.craft.service.BomService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +26,7 @@ import java.util.Map;
 public class ProductController {
     private final ProductRepository productRepository;
     private final BomRepository bomRepository;
+    private final BomService bomService;
 
     @GetMapping("/api/product")
     public ResponseEntity<?> getAllProducts() {
@@ -38,33 +42,16 @@ public class ProductController {
 
     @GetMapping("/api/product/{productId}")
     public ResponseEntity<?> getProductById(@PathVariable String productId) {
-
         Product product = productRepository.findById(productId).orElseThrow();
-
 
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("status", 200);
         response.put("product", product);
 
-        List<Bom> bomList = bomRepository.findByParentProduct(product);
-
-        List<BomNode> bomNodeList =bomList.stream().map( (bom) -> {
-            List<Bom> cilldBomList = bomRepository.findByParentProduct(bom.getChildProduct());
-            List<BomNode> children = cilldBomList.stream().map( (child) -> {
-                return BomNode.builder()
-                        .id(child.getId().toString())
-                        .label(child.getChildProduct().getName() +" x " + child.getQuantity())
-                        .build();
-            }).toList();
-
-            return BomNode.builder()
-                    .id(bom.getId().toString())
-                    .label(bom.getChildProduct().getName() +" x " + bom.getQuantity())
-                    .children(children)
-                    .build();
-        }).toList();
-
-        response.put("bomTree", bomNodeList);
+        List<BomNode> bomNodeList = bomService.makeBomNodes(product);
+        response.put("boms", bomNodeList);
+        List<Demand> demand = bomService.calculateRequiredMaterials(product);
+        response.put("materials", demand);
 
 
         return ResponseEntity.status(200).body(response);
